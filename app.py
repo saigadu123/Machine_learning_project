7dc5049283fc36edfbfb339342a38d24a0ee9427
saikrishna <saikrishnagorantla2001@gmail.com>
2022-06-12 12:52:43 +0530

    update 2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/',methods=['POST','GET'])
 def index():
-    return "My name is sai krishna (Data Scientist)"
+    return "My name is sai krishna (Data Scientist) in Fractal Analytics Banglore"
 
 
 if __name__ == "__main__":
