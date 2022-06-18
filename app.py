7b7a1b551eec85c373e0ae6a15d711f12371bbe0
saikrishna <saikrishnagorantla2001@gmail.com>
2022-06-18 13:44:54 +0530

    Exception and logging done

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask
+from housing.logger import logging
+from housing.exception import HousingException
+import sys
 
 app = Flask(__name__)
 
 @app.route('/',methods=['POST','GET'])
 def index():
+    try:
+        raise Exception("We are testing custom Exception")
+    except Exception as e:
+        housing = HousingException(e,sys)
+        logging.info(housing.error_message)
+        logging.info("we are doing custom logging")
     return "My name is sai krishna Studying in SRM University Chennai"
 
 
