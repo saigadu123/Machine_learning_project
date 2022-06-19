bfc69d995318f0ce1e9a384eff5f57f5a7a5aad0
saikrishna <saikrishnagorantla2001@gmail.com>
2022-06-19 10:13:16 +0530

    new

--- housing/exception/__init__.py
+++ housing/exception/__init__.py
@@ -11,12 +11,12 @@ class HousingException(Exception):
     def get_detailed_error_message(error_message:Exception,error_detail:sys)->str:
         """
         error_message ->Exception Object
-        error_detail -> Object os sys module
+        error_detail -> Object of sys module
         """
         _,_,exec_tb = error_detail.exc_info()
         line_number = exec_tb.tb_frame.f_lineno
         file_name = exec_tb.tb_frame.f_code.co_filename
-        error_message = f"Error occured in scrip:[{file_name}] at line number:[{line_number}] error message:[{error_message}]"
+        error_message = f"Error occured in script:[{file_name}] at line number:[{line_number}] error message:[{error_message}]"
         return error_message
 
     def __str__(self):
